dc6aed21cd939292d8507c400b7988e821443476
Lucas Rafael Araújo Lima <lucasrafael123araujolima@gmail.com>
2022-03-08 12:31:08 -0400

    feat: :sparkles: Discord UI #1

---
+++ src/components/UserInfo/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding: 0 11px 0 16px;
+
+  justify-content: space-between;
+  background-color: var(--secondary);
+
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 1px 0px 0px;
+  z-index: 2;
+`;
